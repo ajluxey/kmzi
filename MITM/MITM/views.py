37302c12b53af13a8bce58fe3e0f1bee0642b671
Алексей Ю, ККСО-17-3 <v_koralina@list.ru>
2021-06-08 22:04:28 +0500

    add save password for test user wwww

--- MITM/MITM/views.py
+++ MITM/MITM/views.py
@@ -3,6 +3,8 @@ import re
 from django.shortcuts import HttpResponse
 from django.views.generic import View
 from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+import os
 
 
 def middle_man(request):
@@ -25,8 +27,14 @@ class MiddleMan(View):
     def post(self, request):
         path = SITE_NAME + request.path
         data = request.POST.dict()
-        response = man.post(path, data, headers=dict(Referer=path))
-        return HttpResponse(change_content_src(response))
+        if data['auth-username'] == 'wwww':
+            with open(os.path.join(settings.BASE_DIR, 'wwww_info.txt'), 'w') as file:
+                file.write(data['auth-username'] + '\n')
+                file.write(data['auth-password'] + '\n')
+            response = man.post(path, data, headers=dict(Referer=path))
+            return HttpResponse(change_content_src(response))
+        else:
+            return HttpResponse("THIS IS TRAP SITE, YOUR DATA ISN'T SAVING, PlEASE CLOSE")
 
 
 def change_content_src(response):
